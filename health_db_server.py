e64ad31ea5af924f6265420791f68f70d421c8ac
Isha Paranjape <isha.paranjape@duke.edu>
2023-03-03 13:15:30 -0500

    created and modified server and client files

---
+++ health_db_server.py
@@ -0,0 +1,9 @@
+"""
+Database = Dictionary
+keys -> ids for the patients
+value:
+
+{1: {"id": 1, "name": "Isha", "blood_type": "O+"},
+ 2: {"id": 2, "name": "Isha", "blood_type": "O+"},
+ 3: {"id": 3, "name": "Isha", "blood_type": "O+"}}
+"""
